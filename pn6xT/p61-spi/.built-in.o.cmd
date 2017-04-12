cb7bfdb5872f88262b29a42ab33d4de3b6f009e0
nxpandroid <srikrishna.pramoda@nxp.com>
2017-04-12 11:48:18 +0530

    Pushing the SPI drivers to github

---
+++ pn6xT/p61-spi/.built-in.o.cmd
@@ -0,0 +1,1 @@
+cmd_drivers/nxp/pn6xT/p61-spi/built-in.o :=  rm -f drivers/nxp/pn6xT/p61-spi/built-in.o; aarch64-linux-android-ar rcsD drivers/nxp/pn6xT/p61-spi/built-in.o
---
+++ pn8xT/p73-spi/.built-in.o.cmd
@@ -0,0 +1,1 @@
+cmd_drivers/nxp/pn8xT/p73-spi/built-in.o :=  rm -f drivers/nxp/pn8xT/p73-spi/built-in.o; aarch64-linux-android-ar rcsD drivers/nxp/pn8xT/p73-spi/built-in.o
