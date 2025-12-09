1dd611845cb8e122f7c5eb2540944b49e30fdb6f
nxf24591 <nanjesh.s_1@nxp.com>
2025-12-10 00:13:14 +0530

    Updated corresponding to - NFC_AR_00_7E800_16.09.00_OpnSrc

--- ese/ese_reset.h
+++ ese/ese_reset.h
@@ -1,6 +1,6 @@
 /******************************************************************************
  *
- *  Copyright 2023 NXP
+ * Copyright 2023-2025 NXP
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
